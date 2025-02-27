e170e696ef3fcb12f961739f7d7ff38e2b7e60d0
xXHenneBXx <59542715+xXHenneBXx@users.noreply.github.com>
2025-02-27 13:23:17 -0500

    Upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,17 @@
+# BACKUP REPLACE FILE
+
+# BACKUP REPLACE FILE
+
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_A15C.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_A15C-user \
+    twrp_A15C-userdebug \
+    twrp_A15C-eng

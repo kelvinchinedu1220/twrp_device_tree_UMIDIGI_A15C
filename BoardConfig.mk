f246d204d3b855dc11488237afbdfc79b9cb7a2d
xXHenneBXx <59542715+xXHenneBXx@users.noreply.github.com>
2025-03-21 11:58:45 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,35 +12,35 @@ ALLOW_MISSING_DEPENDENCIES := true
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    l_agdsp \
-    l_deltanv \
-    l_fixnv1 \
-    l_fixnv2 \
-    l_gdsp \
-    l_ldsp \
-    l_modem \
-    mmcblk0boot1 \
-    pm_sys \
-    sdc \
-    sml \
-    teecfg \
-    trustos \
-    uboot \
-    vbmeta \
-    vbmeta_odm \
-    vbmeta_product \
-    vbmeta_system \
-    vbmeta_system_ext \
-    vbmeta_vendor \
-    vendor_boot \
-    vendor \
-    odm \
-    system \
-    product \
-    vendor_dlkm \
-    system_ext
+boot \
+dtbo \
+l_agdsp \
+l_deltanv \
+l_fixnv1 \
+l_fixnv2 \
+l_gdsp \
+l_ldsp \
+l_modem \
+mmcblk0boot1 \
+odm \
+pm_sys \
+product \
+sdc \
+sml\
+system\
+system_ext \
+teecfg \
+trustos \
+uboot \
+vbmeta \
+vbmeta_odm \
+vbmeta_product \
+vbmeta_system \
+vbmeta_system_ext \
+vbmeta_vendor \
+vendor \
+vendor_boot \
+vendor_dlkm
 BUILDING_VENDOR_BOOT_IMAGE := true
 
 # Architecture

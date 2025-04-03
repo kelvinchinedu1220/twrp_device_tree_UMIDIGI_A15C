78c3cc3403045d725a72d49e8c56813f15b626e1
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-04-03 02:08:31 +0100

    Update twrp_A15C.mk

--- twrp_A15C.mk
+++ twrp_A15C.mk
@@ -1,27 +1,33 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Inherit from A15C
 $(call inherit-product, device/umidigi/A15C/device.mk)
+
+# Inherit some common recovery stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := A15C
 PRODUCT_NAME := twrp_A15C
 PRODUCT_BRAND := UMIDIGI
 PRODUCT_MODEL := MP34
 PRODUCT_MANUFACTURER := UMIDIGI
+
 PRODUCT_GMS_CLIENTID_BASE := android-sunvov
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="TWRP_UMIDIGI_A15C_V1.0_20240314"
-
-BUILD_FINGERPRINT := UMIDIGI/UMIDIGI_A15C/UMIDIGI_A15C:13/TP1A.220624.014/2306241037:user/release-keys
